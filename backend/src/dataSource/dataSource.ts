26a76881d2779aed82ec40041c6f1329c93dd58b
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2024-10-23 16:51:48 +0200

    Creationd 'un jeu de donnée fictive pour les projets

--- backend/src/dataSource/dataSource.ts
+++ backend/src/dataSource/dataSource.ts
@@ -1,4 +1,5 @@
 import { DataSource } from "typeorm";
+import { Project } from "../entities/Project";
 
 const dbHost: string = process.env.DB_HOST || "";
 const dbPort: number = Number.parseInt(process.env.DB_PORT || "", 10);
@@ -17,3 +18,38 @@ export const dataSource = new DataSource({
 	synchronize: true,
 	logging: "all",
 });
+
+export async function cleanDB() {
+	await dataSource.manager.clear(Project);
+}
+
+export async function initTestData(
+	name: string,
+	description: string,
+	startDate: Date,
+	endDate: Date,
+) {
+	const project1 = new Project(
+		"Project One",
+		"Ceci est mon premier projet",
+		new Date("2024-10-23"),
+		new Date("2025-10-23"),
+	);
+
+	const project2 = new Project(
+		"Project Two",
+		"Un super projet",
+		new Date("2024-10-23"),
+		new Date("2025-10-23"),
+	);
+
+	const project3 = new Project(
+		"Project Three",
+		"Encore meilleur",
+		new Date("2024-10-23"),
+		new Date("2025-10-23"),
+	);
+	await dataSource.manager.save(project1);
+	await dataSource.manager.save(project2);
+	await dataSource.manager.save(project3);
+}
