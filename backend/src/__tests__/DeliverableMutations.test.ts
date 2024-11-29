8d377d8530554c60de4c8783c1c3595bd44cd3b8
AzraeLandres <azraellandres@gmail.com>
2024-11-29 09:02:28 +0100

    error fix

---
+++ backend/src/__tests__/DeliverableMutations.test.ts
@@ -0,0 +1,9 @@
+import { Status } from "../entities/Task";
+import { faker } from "@faker-js/faker";
+import { Deliverable } from "../entities/Deliverable";
+import { DeliverableMutations } from "../graphql-resolvers/DeliverableMutations";
+import { mockTypeOrm } from "../__tests_mockTypeorm-config";
+
+describe("Deliverable creation", () => {
+  let deliverableMutations: DeliverableMutations;
+});
