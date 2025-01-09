cd41dc2fd031bbac0442e56ccbe2cf3fb79fc1c4
AzraeLandres <azraellandres@gmail.com>
2025-01-09 17:38:11 +0100

    Added icons as components

---
+++ frontend/src/Components/Icons/PlusIcon.tsx
@@ -0,0 +1,17 @@
+export default function ProjectsIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M6.5 8.83789H0.5V6.83789H6.5V0.837891H8.5V6.83789H14.5V8.83789H8.5V14.8379H6.5V8.83789Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
