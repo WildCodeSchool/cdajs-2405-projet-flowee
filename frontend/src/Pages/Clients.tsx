bf06d265d36d7fa333a1d0959952fa74e93d83eb
AzraeLandres <azraellandres@gmail.com>
2025-01-09 15:13:21 +0100

    added pages

---
+++ frontend/src/Pages/Clients.tsx
@@ -0,0 +1,7 @@
+export default function Clients() {
+  return (
+    <div>
+      <h1>Welcome to the Clients Page</h1>
+    </div>
+  );
+}
---
+++ frontend/src/Pages/Settings.tsx
@@ -0,0 +1,7 @@
+export default function Settings() {
+  return (
+    <div>
+      <h1>Welcome to the Settings Page</h1>
+    </div>
+  );
+}
