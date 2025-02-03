3b434701d6a4031ac6d1eb3afc49511984567b04
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2025-02-03 20:58:48 +0100

    Fix pages

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
-docker compose --env-file .env -f docker-compose.dev.yml up --build -d --force-recreate
+docker compose  -f docker-compose.dev.yml up --build -d --force-recreate
 
 
-docker compose --env-file .env -f docker-compose.prod.yml up --build -d --force-recreate
+docker compose  -f docker-compose.prod.yml up --build -d --force-recreate
 
-docker compose --env-file .env -f docker-compose.staging.yml up --build -d --force-recreate
+docker compose  -f docker-compose.staging.yml up --build -d --force-recreate
 
 docker compose -f docker-compose.prod.yml down -v && \ docker compose -f docker-compose.prod.yml pull && \
 
 docker compose -f docker-compose.staging.yml down && \ docker compose -f docker-compose.staging.yml pull && \
+
+
