934874581ae56d47ba247149b88febabbd7230fa
AzraeLandres <azraellandres@gmail.com>
2025-02-05 17:44:41 +0100

    init searchbar

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+export default function SearchBar() {
+  return (
+    <div className="bg-black">
+      <input
+        type="text"
+        placeholder="Looking for someting unique?"
+        className="p-2 border bg-red-800 border-gray-300 rounded"
+      />
+    </div>
+  );
+}
