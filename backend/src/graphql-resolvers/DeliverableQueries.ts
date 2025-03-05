a883215196ef90dda769cc3b7cdbe64451aee9fe
LuisMiLR <luismiguel.robles.lr@gmail.com>
2025-03-05 09:31:39 +0100

    udpate DeliverableQueries ProjectQueries

--- backend/src/graphql-resolvers/DeliverableQueries.ts
+++ backend/src/graphql-resolvers/DeliverableQueries.ts
@@ -1,24 +1,23 @@
 import { Arg, Query, Resolver } from "type-graphql";
-import { Deliverable } from "../entities/Deliverable";
 import { dataSource } from "../dataSource/dataSource";
+import { Deliverable } from "../entities/Deliverable";
 
 @Resolver(Deliverable)
 export class DeliverableQueries {
-  @Query((type) => [Deliverable])
+  @Query(() => [Deliverable])
   async getAllDeliverables(): Promise<Deliverable[]> {
-    const deliverable: Deliverable[] = await dataSource.manager.find(
-      Deliverable
-    );
+    const deliverable: Deliverable[] =
+      await dataSource.manager.find(Deliverable);
     return deliverable;
   }
 
-  @Query((type) => Deliverable, { nullable: true })
+  @Query(() => Deliverable, { nullable: true })
   async getDeliverable(@Arg("id") id: number): Promise<Deliverable | null> {
     const deliverable: Deliverable | null = await dataSource.manager.findOne(
       Deliverable,
       {
         where: { id },
-      }
+      },
     );
     return deliverable;
   }
--- backend/src/graphql-resolvers/ProjectQueries.ts
+++ backend/src/graphql-resolvers/ProjectQueries.ts
@@ -4,7 +4,7 @@ import { dataSource } from "../dataSource/dataSource";
 
 @Resolver(Project)
 export class ProjectQueries {
-  @Query((type) => [Project])
+  @Query(() => [Project])
   async getAllProjects(): Promise<Project[]> {
     const projects: Project[] = await dataSource.manager.find(Project);
     return projects;
