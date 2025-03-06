453e4b7460a5511d3c2b02fbb08f50d994ecf8c0
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2025-03-06 10:22:26 +0100

    Update tests on ProjectQueries and Mutations

--- backend/src/__tests__/ProjectMutations.test.ts
+++ backend/src/__tests__/ProjectMutations.test.ts
@@ -2,42 +2,72 @@ import { faker } from "@faker-js/faker";
 import { mockTypeOrm } from "../__tests_mockTypeorm-config";
 import { Project } from "../entities/Project";
 import { ProjectMutations } from "../graphql-resolvers/ProjectMutations";
+import { Status } from "../enums/Status";
 
 describe("Project creation", () => {
-	let projectMutations: ProjectMutations;
-	let project: Project;
+  let projectMutations: ProjectMutations;
+  let project: Project;
+  // let mockCtx: any;
 
-	beforeEach(() => {
-		projectMutations = new ProjectMutations();
+  beforeEach(() => {
+    projectMutations = new ProjectMutations();
 
-		project = new Project(
-			faker.commerce.productName(),
-			faker.person.fullName(),
-			faker.lorem.sentence(),
-			faker.date.past().toString(),
-			faker.date.future().toString(),
-		);
-	});
+    // mockCtx = { user: { id: faker.number.int({ min: 1, max: 1000 }) } };
 
-	describe("create project", () => {
-		it("should create a project ", async () => {
-			mockTypeOrm().onMock(Project).toReturn(project, "save");
-			console.info("project", project);
-			const createdProject: Project = await projectMutations.createProject(
-				project.name,
-				project.author,
-				project.description,
-				project.startDate,
-				project.endDate,
-			);
-			console.info("dans les tests, projet recupéré", createdProject);
-			expect(createdProject).toMatchObject({
-				name: project.name,
-				author: project.author,
-				description: project.description,
-				startDate: project.startDate,
-				endDate: project.endDate,
-			});
-		});
-	});
+    project = new Project(
+      faker.commerce.productName(),
+      faker.internet.email(),
+      2,
+      faker.lorem.sentence(),
+      new Date().toISOString(), // startDate
+      faker.date.future().toISOString(),
+      Status.NOT_STARTED,
+    );
+    console.log(project.status);
+  });
+
+  describe("create project", () => {
+    it("should create a project ", async () => {
+      mockTypeOrm().onMock(Project).toReturn(project, "save");
+      console.info("project mock", project);
+      const createdProject: Project = await projectMutations.createProject(
+        project.name,
+        project.clientEmail,
+        project.description,
+        project.endDate,
+      );
+      console.info("dans les tests, projet créé", createdProject);
+      expect(createdProject).toMatchObject({
+        name: project.name,
+        clientEmail: project.clientEmail,
+        companyUserId: project.companyUserId,
+        description: project.description,
+        startDate: expect.any(String),
+        endDate: project.endDate,
+        status: project.status,
+      });
+    });
+
+    it("should fail if clientEmail is missing", async () => {
+      await expect(
+        projectMutations.createProject(
+          project.name,
+          "",
+          project.description,
+          project.endDate,
+        ),
+      ).rejects.toThrow("Validation error");
+    });
+
+    it("should fail if no name is provided", async () => {
+      await expect(
+        projectMutations.createProject(
+          "",
+          project.clientEmail,
+          project.description,
+          project.endDate,
+        ),
+      ).rejects.toThrow("Validation error");
+    });
+  });
 });
--- backend/src/__tests__/ProjectQueries.test.ts
+++ backend/src/__tests__/ProjectQueries.test.ts
@@ -9,36 +9,16 @@ describe("Project Graphql queries", () => {
 
   beforeEach(() => {
     projectQueries = new ProjectQueries();
-    projects = [
-      new Project(
+    projects = Array.from({ length: 4 }).map(() => {
+      return new Project(
         faker.commerce.productName(),
-        faker.person.fullName(),
+        faker.internet.email(),
+        faker.number.int({ min: 1, max: 1000 }),
         faker.lorem.sentence(),
-        faker.date.past().toString(),
-        faker.date.future().toString(),
-      ),
-      new Project(
-        faker.commerce.productName(),
-        faker.person.fullName(),
-        faker.lorem.sentence(),
-        faker.date.past().toString(),
-        faker.date.future().toString(),
-      ),
-      new Project(
-        faker.commerce.productName(),
-        faker.person.fullName(),
-        faker.lorem.sentence(),
-        faker.date.past().toString(),
-        faker.date.future().toString(),
-      ),
-      new Project(
-        faker.commerce.productName(),
-        faker.person.fullName(),
-        faker.lorem.sentence(),
-        faker.date.past().toString(),
-        faker.date.future().toString(),
-      ),
-    ];
+        faker.date.past().toISOString(),
+        faker.date.future().toISOString(),
+      );
+    });
   });
 
   describe("query all projects from TypeORM", () => {
@@ -48,10 +28,11 @@ describe("Project Graphql queries", () => {
         await projectQueries.getAllProjects();
       expect(retrievedProjects.length).toBe(projects.length);
       expect(retrievedProjects[0]).toHaveProperty("name");
-      expect(retrievedProjects[0]).toHaveProperty("author");
+      expect(retrievedProjects[0]).toHaveProperty("clientEmail");
       expect(retrievedProjects[0]).toHaveProperty("description");
       expect(retrievedProjects[0]).toHaveProperty("startDate");
       expect(retrievedProjects[0]).toHaveProperty("endDate");
+      expect(retrievedProjects[0]).toHaveProperty("companyUserId");
     });
   });
 });
