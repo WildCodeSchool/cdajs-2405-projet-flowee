e6c2748f6712b91fd9f467150d56f050d6b6a988
Azrael Landres <azraellandres@gmail.com>
2025-04-08 17:54:01 +0200

    refactored Login to follow atomic design systeme

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,85 +1,48 @@
-import { useForm } from "react-hook-form";
-import {
-  type LoginMutation,
-  useLoginMutation,
-} from "../__generated__/graphql-types";
-import type { ApolloError } from "@apollo/client";
-import { useNavigate } from "react-router";
-import { useAuth } from "../context/authContext";
+import { NavLink } from "react-router-dom";
 import LogoIcon from "../components/Icons/Logo";
-
-interface LoginFormData {
-  email: string;
-  password: string;
-}
-
+import LogoMarkIcon from "../components/Icons/LogoMark";
+import AuthIllustration from "../components/illustrations/AuthIllus";
+import LoginForm from "../molecules/LoginForm";
 export default function Login() {
-  const { setToken } = useAuth();
-  const navigate = useNavigate();
-
-  const [sendLoginMutation, { loading, error }] = useLoginMutation({
-    onCompleted: (data: LoginMutation) => {
-      const token: string = data.login;
-      setToken(token);
-      // localStorage.setItem("AUTH_TOKEN", token);
-      navigate("/dashboard");
-    },
-    onError: (error: ApolloError) => {
-      console.error("login failed", error);
-    },
-  });
-
-  const { handleSubmit, register } = useForm<LoginFormData>();
-
-  const LoginFormSubmitted = (formData: LoginFormData) => {
-    sendLoginMutation({
-      variables: formData,
-    });
-  };
-
   return (
-    <div className="flex flex-row w-full">
-      <div className="w-[50%]  bg-verylightorange"></div>
-      <div className=" flex flex-col items-center gap-10 h-svh justify-centerpx-10">
-        <LogoIcon className="h-40 w-40" />
-        <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-3xl font-bold">Welcome back to Flowee ! </h1>
-          <h2>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
-            rerum iusto quisquam explicabo!
+    <div className="flex flex-row w-full ">
+      <aside className=" hidden sm:block sm:bg-orangeLight sm:w-[55%] sm:p-10">
+        <NavLink to="/">
+          {" "}
+          <LogoIcon className=" w-40" />
+        </NavLink>
+        <AuthIllustration className="w-[80%] mx-auto" />
+      </aside>
+      <section className=" flex flex-auto flex-col bg-white gap-2 h-svh sm:justify-center sm:px-8 lg:px-28 justify-end">
+        <div className="flex flex-col gap-3 text-center">
+          <aside className="flex flex-col sm:flex-row items-center gap-2">
+            <LogoMarkIcon className=" w-12 sm:w-8 " />
+            <h1 className=" text-2xl sm:text-3xl font-bold ">SIGN IN</h1>
+          </aside>
+
+          <h2 className="hidden sm:flex sm:text-lg sm:text-left ">
+            Welcome back! <br />
+            Everything’s ready – just log in and pick up where you left off.
           </h2>
         </div>
-        <div className="bg-white flex flex-col items-center gap-10 rounded-t-[3rem] py-10">
-          <div className="flex flex-col gap-4  max-w-100 justify-self-center">
-            <form
-              onSubmit={handleSubmit(LoginFormSubmitted)}
-              className="flex flex-col gap-1  justify-self-center"
-            >
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                className="w-full py-2 px-4 border border-gray rounded-md"
-                {...register("email", { required: true })}
-                placeholder=""
-              />{" "}
-              <br />
-              <label htmlFor="password">Password</label>
-              <input
-                className="w-full py-2 px-4 border border-gray rounded-md"
-                type="password"
-                {...register("password", { required: true })}
-                placeholder="Mot de passe"
-              />{" "}
-              <br />
-              <input type="submit" value="Se connecter" />
-              {loading && "Loading..."}
-              <br />
-              {error && "Une erreur est survenue, merci de réessayer..."}
-              <br />
-            </form>
+        <div className=" mt-6 relative z-0 sm:w-full">
+          <div className=" sm:hidden h-36 bg-[#E9BB8E] rounded-t-[43px]" />
+          <div className=" sm:hidden h-36 bg-[#8597D0] rounded-t-[43px] -mt-8 z-10 relative" />
+          <div className="bg-white rounded-t-[43px] -mt-8 z-20 relative py-20 p-8 sm:py-6 sm:px-0">
+            <LoginForm />
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
+}
+
+{
+  /* <div className="bg-white flex flex-col items-center gap-10 rounded-t-[3rem] py-5 mt-6 relative z-0">
+<div className="h-32 bg-warning rounded-t-[32px]" />
+<div className="h-32 bg-blue rounded-t-[32px] -mt-8 z-10 relative" />
+<div className="bg-white rounded-t-[32px] -mt-8 z-20 relative px-6 pb-6">
+  <LoginForm />
+</div>
+</div> */
 }
