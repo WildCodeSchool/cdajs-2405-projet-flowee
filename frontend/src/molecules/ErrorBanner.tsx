a0430fbfec6a424a8b48ed4d22c3791b0cc40a23
Azrael Landres <azraellandres@gmail.com>
2025-04-08 22:59:23 +0200

    made fill color dynamic

--- frontend/src/molecules/ErrorBanner.tsx
+++ frontend/src/molecules/ErrorBanner.tsx
@@ -10,7 +10,7 @@ export default function ErrorBanner({
     <div
       className={`flex items-center gap-3 bg-lightRed p-3 rounded-md border border-red`}
     >
-      <WarningIcon className="w-5 h-5 mr-2" color="#d9495b" />
+      <WarningIcon className="w-5 h-5 mr-2 fill-theme-error" />
       <p className="font-medium">{message}</p>
     </div>
   );
