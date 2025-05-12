4c76ea0153212eaa03045bbdb525e0bcbb6489c9
Azrael Landres <azraellandres@gmail.com>
2025-05-12 16:36:29 +0200

    fixed fill for plus icon

--- frontend/src/components/organisms/Navigation.tsx
+++ frontend/src/components/organisms/Navigation.tsx
@@ -47,7 +47,7 @@ export default function Navigation() {
               className="flex flex-col items-center text-gray-500 md:justify-center"
             >
               <div className="flex items-center justify-center w-12 h-12 md:w-8 md:h-8 bg-theme-btnBG rounded-full hover:bg-theme-veryDark">
-                <PlusIcon className="h-6 w-6 md:h-4 md:w-4 text-white" />
+                <PlusIcon className="h-6 w-6 md:h-4 md:w-4 fill-white" />
               </div>
             </NavLink>
           </li>
