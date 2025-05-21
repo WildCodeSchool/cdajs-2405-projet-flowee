c136b7db2f51ee0b10f12f1e7aab1b75c5ad2e22
LuisMiLR <luismiguel.robles.lr@gmail.com>
2025-05-21 21:54:18 +0200

    update focus in settingsCompanyUser

--- frontend/src/components/organisms/SettingsCompanyUser.tsx
+++ frontend/src/components/organisms/SettingsCompanyUser.tsx
@@ -1,10 +1,10 @@
-import { useMeQuery} from "@generated/graphql-types";
+import { useMeQuery } from "@generated/graphql-types";
 
 export default function Settings() {
   const { data, loading, error } = useMeQuery();
 
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error: {error.message}</div>
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
   if (!data?.me) return <div>User not found or not a company user</div>;
 
   const user = data?.me.companyUser;
@@ -41,8 +41,8 @@ export default function Settings() {
                   id="company"
                   name="company"
                   type="text"
-                  className="w-full rounded border border-theme-gray px-3 py-1 focus:outline-theme-base  focus:ring-2 focus:ring-orange-300"
-                  value={company?.name || ''}
+                  className="w-full rounded border border-theme-gray px-3 py-2 focus:ring-1 focus:ring-theme-light focus:outline-none"
+                  value={company?.name || ""}
                   readOnly
                 />
               </div>
@@ -57,8 +57,8 @@ export default function Settings() {
                   id="role"
                   name="role"
                   type="text"
-                  className="w-full rounded border border-theme-gray px-3 py-1 focus:outline-theme-base  focus:ring-2 focus:ring-orange-300"
-                  value={account?.role || ''}
+                  className="w-full rounded border border-theme-gray px-3 py-2 focus:ring-1 focus:ring-theme-light focus:outline-none"
+                  value={account?.role || ""}
                   readOnly
                 />
               </div>
@@ -73,8 +73,8 @@ export default function Settings() {
                   id="firstName"
                   name="firstName"
                   type="text"
-                  className="w-full rounded border border-theme-gray px-3 py-1 focus:outline-theme-base  focus:ring-2 focus:ring-orange-300"
-                  value={user?.firstname || ''}
+                  className="w-full rounded border border-theme-gray px-3 py-2 focus:ring-1 focus:ring-theme-light focus:outline-none"
+                  value={user?.firstname || ""}
                   readOnly
                 />
               </div>
@@ -89,8 +89,8 @@ export default function Settings() {
                   id="lastName"
                   name="lastName"
                   type="text"
-                  className="w-full rounded border border-theme-gray px-3 py-1 focus:outline-theme-base  focus:ring-2 focus:ring-orange-300"
-                  value={user?.lastname || ''}
+                  className="w-full rounded border border-theme-gray px-3 py-2 focus:ring-1 focus:ring-theme-light focus:outline-none"
+                  value={user?.lastname || ""}
                   readOnly
                 />
               </div>
@@ -105,8 +105,8 @@ export default function Settings() {
                   id="email"
                   name="email"
                   type="email"
-                  className="w-full rounded border border-theme-gray px-3 py-1 focus:outline-theme-base  focus:ring-2 focus:ring-orange-300"
-                  value={account?.email || ''}
+                  className="w-full rounded border border-theme-gray px-3 py-2 focus:ring-1 focus:ring-theme-light focus:outline-none"
+                  value={account?.email || ""}
                   readOnly
                 />
               </div>
@@ -114,7 +114,10 @@ export default function Settings() {
           </section>
 
           <section aria-labelledby="notifications-heading" className="mb-12">
-            <h2 id="notifications-heading" className="text-xl font-semibold mb-6">
+            <h2
+              id="notifications-heading"
+              className="text-xl font-semibold mb-6"
+            >
               Notifications
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-2 md:ml-6">
@@ -176,6 +179,6 @@ export default function Settings() {
           </div>
         </div>
       </div>
-</div>
+    </div>
   );
 }
