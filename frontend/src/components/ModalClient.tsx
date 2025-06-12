316f76b5e0b1e81953e2de1e6374116a2f023e8f
LuisMiLR <luismiguel.robles.lr@gmail.com>
2025-06-12 22:40:46 +0200

    added projects button in modal client

--- frontend/src/components/ModalClient.tsx
+++ frontend/src/components/ModalClient.tsx
@@ -1,186 +1,168 @@
-import { Input } from "@atoms/Input";
+import { Avatar } from "@atoms/Avatar";
+import EditBtn from "@atoms/Icons/editBtn";
 import {
-  ClientStatus,
-  useUpdateClientMutation,
+  useGetProjectsByUserQuery,
+  useMeQuery,
 } from "@generated/graphql-types";
-import SuccessBanner from "@molecules/SuccesBanner";
 import { useState } from "react";
-import ErrorBanner from "./molecules/ErrorBanner";
-import { capitalize } from "@utils/stringUtils";
+import PasswordModal from "@molecules/PasswordModal";
 
-interface ModalClientProps {
-  id: number;
-  currentName?: string | null;
-  currentEmail: string;
-  currentStatus?: ClientStatus | null;
-  currentProjects?: string[];
-  onClose: () => void;
-}
+export default function SettingsClient() {
+  const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
+  const { data, loading, error } = useMeQuery();
+  const {
+    data: projectsData,
+    loading: projectsLoading,
+    error: projectsError,
+  } = useGetProjectsByUserQuery();
 
-export default function ModalClient({
-  id,
-  currentName,
-  currentEmail,
-  currentStatus,
-  currentProjects = [],
-  onClose,
-}: ModalClientProps) {
-  const [name, setName] = useState(currentName ?? "");
-  const [email, setEmail] = useState(currentEmail || "");
-  const [status, setStatus] = useState<ClientStatus>(
-    currentStatus || ClientStatus.Active,
-  );
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data?.me) return <div>User not found.</div>;
 
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  if (projectsLoading) return <div>Loading...</div>;
+  if (projectsError) return <div>Error: {projectsError.message}</div>;
 
-  const [updateClient, { loading }] = useUpdateClientMutation({
-    onCompleted: () => {
-      setSuccessMessage("Client updated successfully.");
-      setTimeout(() => {
-        onClose();
-      }, 1500);
-    },
-    onError: (error) => {
-      console.error("Update failed:", error);
-      setErrorMessage("Update failed. Please try again.");
-    },
-  });
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    updateClient({
-      variables: {
-        id: id,
-        newName: name,
-        newEmail: email,
-        newStatus: status.toString(),
-      },
-    });
-  };
-  //TODO: les commentaires sont hardcodés et seront remplacés par une query dès que la fonctionnalité sera implémentée
-  const hardcodedComments = [
-    { text: "I have a lot to say.." },
-    { text: "This isn't a perfect world" },
-  ];
+  const client = data.me.client;
+  const account = data.me;
+  const projects = projectsData?.getProjectsByUser || [];
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-end bg-black bg-opacity-20 backdrop-blur-sm">
-      <div
-        className="absolute inset-0"
-        onClick={onClose}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" || e.key === " ") {
-            onClose();
-          }
-        }}
-        tabIndex={0}
-        role="button"
-        aria-label="Close modal"
-      />
-
-      <div className="relative z-50 bg-white p-6 rounded-l-md w-[400px] max-w-full h-full">
-        <h2 className="text-xl font-medium mb-6">Edit Client</h2>
-        <button
-          type="button"
-          onClick={onClose}
-          className="absolute top-3 right-6 font-semibold text-gray-500 hover:text-gray-700"
-        > 
-        x
-        </button>
-
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <Input
-            label="Name"
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Enter name"
-          />
-
-          <Input
-            label="Email"
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter email"
-          />
-
-          <div>
-            <label htmlFor="projects" className="block text-base mb-1">
-              Projects
-            </label>
-            <div className="w-full py-2 px-4 bg-lightgray rounded-md focus:bg-white focus:outline-blue focus:invalid:border-red focus:invalid:outline-red">
-              {currentProjects && currentProjects.length > 0 ? (
-                currentProjects.map((project, index) => (
-                  <div
-                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                    key={index}
-                    className="bg-orange-100 text-orange-800 rounded px-3 py-1 inline-block mr-2 mb-2"
+    <div>
+      <h1 className="text-2xl font-bold mb-[25px]">Settings</h1>
+      <main className="w-full max-w-full mx-auto flex flex-col md:flex-row items-start">
+        <div className="w-full md:w-auto mb-8 md:mb-0 flex-shrink-0">
+          <div
+            className="relative inline-block px-3 py-2 bg-theme-veryLight text-black font-semibold
+              before:content-[''] before:absolute before:right-0 before:top-0 before:bottom-0 before:w-1 before:bg-theme-light"
+            style={{ minWidth: 140 }}
+          >
+            Account
+          </div>
+        </div>
+        <div className="flex-1 w-full max-w-4xl md:ml-12">
+          <div className="flex items-center gap-6 mb-10">
+            <Avatar name={`${client?.clientName || ""}`} size="lg" />
+            <div>
+              <h3 className="text-lg font-semibold">{client?.clientName}</h3>
+              <button
+                type="button"
+                className="mt-1 px-3 py-1 border border-gray bg-theme-lightGray hover:bg-zinc-200 rounded-md text-theme-darkGray text-sm font-medium"
+              >
+                Upload profile picture
+              </button>
+            </div>
+          </div>
+          <section aria-labelledby="profile-heading" className="mb-4">
+            <h2 id="profile-heading" className="text-xl font-semibold mb-6">
+              Profile
+            </h2>
+            <form className="space-y-6 md:ml-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-base font-medium mb-1"
                   >
-                    {project}
-                  </div>
-                ))
-              ) : (
-                <span className="text-gray-500 italic">
-                  No projects assigned
-                </span>
-              )}
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-1">
-            <label htmlFor="status">Status</label>
-            <select
-              id="status"
-              value={status}
-              onChange={(e) => setStatus(e.target.value as ClientStatus)}
-              className="w-full py-2 px-4 bg-lightgray rounded-md focus:bg-white focus:outline-blue"
-            >
-              {Object.values(ClientStatus).map((statusOption) => (
-                <option key={statusOption} value={statusOption}>
-                  {capitalize(statusOption)}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <h3 className="text-base mb-3">Comments</h3>
-            <div className="space-y-4">
-              {hardcodedComments.map((comment, index) => (
-                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                <div key={index} className="flex items-start">
-                  <div className=" flex-shrink-0 w-8 h-8 bg-orange-100 rounded-full flex items-center justify-center mr-3 text-orange-500 text-sm">
-                    CD
-                  </div>
-                  <p className="mt-[6px] text-sm text-gray-700">
-                    {comment.text}
-                  </p>
+                    Email
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    value={account?.email || ""}
+                    readOnly
+                    className="w-full rounded border border-theme-gray px-3 py-2 focus:ring-1 focus:ring-theme-light focus:outline-none"
+                  />
                 </div>
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-base font-medium mb-1"
+                  >
+                    Password
+                  </label>
+                  <div className="relative">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      value={"********"}
+                      readOnly
+                      className="w-full rounded border border-theme-gray px-3 py-2 pr-5 focus:ring-1 focus:ring-theme-light focus:outline-none"
+                    />
+                    <button
+                      type="button"
+                      className="absolute right-2 top-1/2 -translate-y-1/2 text-theme-light hover:text-theme-dark font-medium"
+                      aria-label="Edit password"
+                      onClick={() => setIsPasswordModalOpen(true)}
+                    >
+                      <EditBtn className="w-6 h-6" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </section>
+          <section aria-labelledby="project-heading" className="mb-10">
+            <h2 id="project-heading" className="md:ml-6 text-base font-medium ">
+              Projects
+            </h2>
+            <div className="md:ml-6 space-y-2">
+              {projects.length === 0 && (
+                <div className="text-gray-400 text-sm">No project found</div>
+              )}
+              {projects.map((project) => (
+                <button
+                  key={project.id}
+                  type="button"
+                  className="border border-[#D4711D] bg-[#FAF1E7] text-[#3A3631] text-sm font-medium rounded-md px-3 py-1 shadow-none hover:bg-[#f9d9b5] transition-colors mr-2"
+                >
+                  {project.projectName}
+                </button>
               ))}
             </div>
-          </div>
-          <div className="h-12 mt-2">
-            {successMessage && <SuccessBanner message={successMessage} />}
-            {errorMessage && <ErrorBanner message={errorMessage} />}
-          </div>
-
-          <div className="pt-0">
-            <button
-              type="submit"
-              disabled={loading}
-              className="w-[208px] h-[40px] bg-theme-veryDark hover:bg-orange-700 text-white rounded-lg py-2 font-medium"
+          </section>
+          <section aria-labelledby="notifications-heading" className="mb-12">
+            <h2
+              id="notifications-heading"
+              className="text-xl font-semibold mb-6"
             >
-              {loading ? "Saving changes..." : "Save changes"}
+              Notifications
+            </h2>
+            <div className="flex flex-col space-y-4 md:ml-6">
+              <label className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-4 h-4 text-theme-light"
+                  defaultChecked
+                />
+                <span>Deliverable needs approval</span>
+              </label>
+              <label className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  className="w-4 h-4 text-theme-light"
+                  defaultChecked
+                />
+                <span>Company saw a comment</span>
+              </label>
+            </div>
+          </section>
+          <div className="flex justify-start">
+            <button
+              type="button"
+              className="w-[245px] h-[40px] bg-red hover:bg-rose-700 text-white rounded-lg py-2 font-medium mb-10"
+            >
+              Request account deletion
             </button>
           </div>
-        </form>
-      </div>
+        </div>
+      </main>
+      <PasswordModal
+        isOpen={isPasswordModalOpen}
+        onClose={() => setIsPasswordModalOpen(false)}
+      />
     </div>
   );
 }
