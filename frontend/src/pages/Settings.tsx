bf429ce88e3f46a9d4e1cf9123257696a7e7eb57
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2025-06-12 11:36:36 +0200

    Update settings componenent

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -2,31 +2,34 @@ import SignedInLayout from "@layout/SignedInLayout";
 import { useMeQuery } from "@generated/graphql-types";
 import SettingsCompanyUser from "@organisms/SettingsCompanyUser";
 import SettingsClient from "@organisms/SettingsClient";
-import { useAuth } from "@context/authContext";
 
 export default function Settings() {
   const { data, loading, error } = useMeQuery();
-
-  const { authUserData } = useAuth();
-  console.info("Auth user data in Settings:", authUserData);
-
   console.info("data", data);
+  if (loading)
+    return (
+      <SignedInLayout>
+        <div>Loading...</div>
+      </SignedInLayout>
+    );
+  if (error)
+    return (
+      <SignedInLayout>
+        <div>Error: {error.message}</div>
+      </SignedInLayout>
+    );
+  if (!data?.me)
+    return (
+      <SignedInLayout>
+        <div>User not found.</div>
+      </SignedInLayout>
+    );
 
   return (
     <SignedInLayout>
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error.message}</div>}
-      {!loading && !error && !data?.me && <div>User not found.</div>}
-      {!loading && !error && authUserData.role === "ADMIN"
-        ? data?.me?.companyUser && <SettingsCompanyUser />
-        : data?.me?.client && <SettingsClient />}
-
-      {/* Optionnel : fallback si aucun type reconnu */}
-      {!loading &&
-        !error &&
-        data?.me &&
-        !data.me.companyUser &&
-        !data.me.client && <div>Unknown user type</div>}
+      {data.me.companyUser && <SettingsCompanyUser />}
+      {data.me.client && <SettingsClient />}
+      {!data.me.companyUser && !data.me.client && <div>Unknown user type</div>}
     </SignedInLayout>
   );
 }
